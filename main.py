86187fa0017a7745e087231deae72b72560d38cf
Timothy Foster <tf.auroratide@gmail.com>
2026-05-24 21:29:20 -1000

    Use silero to implement a sliding phrase window

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
 import sounddevice as sd
 from faster_whisper import WhisperModel
+from silero_vad import VADIterator
 import threading
 import queue
 import numpy as np
+import torch
 
 SAMPLE_RATE = 16000
+BLOCKSIZE = 512
 CHUNK_SECONDS = 2
 stt_model = WhisperModel("base", device="cpu")
+vad_model, _ = torch.hub.load(repo_or_dir="snakers4/silero-vad", model="silero_vad")
+vad_iterator = VADIterator(model=vad_model, sampling_rate=SAMPLE_RATE, min_silence_duration_ms=CHUNK_SECONDS * 1000)
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -25,15 +30,28 @@ def read_audio(indata, frames, time, status):
 
 def process_audio():
 	buffer = np.array([], dtype=np.float32)
-	with sd.InputStream(channels=1, samplerate=SAMPLE_RATE, callback=read_audio):
+	phrase_buffer = np.array([], dtype=np.float32)
+	with sd.InputStream(channels=1, samplerate=SAMPLE_RATE, blocksize=BLOCKSIZE, callback=read_audio):
+		end_detected = False
 		while True:
 			chunk = audio.get()
+			tensor = torch.from_numpy(chunk.flatten())
+			speech_dict = vad_iterator(tensor, return_seconds=True)
+			if speech_dict and speech_dict.get("end"):
+				print("end detected")
+				end_detected = True
 			buffer = np.concatenate([buffer, chunk.flatten()])
-			if len(buffer) > SAMPLE_RATE * CHUNK_SECONDS:
-				segments, _ = stt_model.transcribe(buffer, vad_filter=True)
+			phrase_buffer = np.concatenate([phrase_buffer, chunk.flatten()])
+
+			if end_detected or len(buffer) > SAMPLE_RATE * CHUNK_SECONDS:
+				segments, _ = stt_model.transcribe(phrase_buffer, vad_filter=True, condition_on_previous_text=False)
 				text = " ".join(s.text for s in segments)
 				print(text)
 				buffer = np.array([], dtype=np.float32)
+				if end_detected:
+					phrase_buffer = np.array([], dtype=np.float32)
+					vad_iterator.reset_states()
+					end_detected = False
 
 def main():
 	print(sd.query_devices())
